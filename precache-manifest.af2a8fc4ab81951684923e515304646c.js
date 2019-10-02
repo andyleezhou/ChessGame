b719ebfc0a0a84968f918f681fbd83b15449bb09
Andy Zhou <andy.lee.zhou@gmail.com>
2019-10-02 15:45:51 -0700

    Updates

---
+++ precache-manifest.af2a8fc4ab81951684923e515304646c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbef51f46b84c46fd165f7c3b5bff687",
+    "url": "/chessGame/index.html"
+  },
+  {
+    "revision": "fbac9583af6abce5e4cf",
+    "url": "/chessGame/static/css/main.c36c262d.chunk.css"
+  },
+  {
+    "revision": "34da199b0c275b4446d0",
+    "url": "/chessGame/static/js/2.de9337e2.chunk.js"
+  },
+  {
+    "revision": "fbac9583af6abce5e4cf",
+    "url": "/chessGame/static/js/main.9be49055.chunk.js"
+  },
+  {
+    "revision": "fd8f50dade33530f7f1a",
+    "url": "/chessGame/static/js/runtime~main.d12c9b26.js"
+  }
+]);
